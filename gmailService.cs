2d37ca64b97790827a89f355d2fc1925aa605d22
Lei (Leo) Zhao <Leo@zhaodeveloper.com>
2018-03-12 09:47:56 +1100

    follow the tutor
    Not safe as the Gmail address and password are hardcode

--- gmailService.cs
+++ gmailService.cs
@@ -1,1 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmailService.Models;
+using System.Net;
+using System.Net.Mail;
 
+namespace EmailService.Controllers
+{
+    public class EmailSetupController : Controller
+    {
+        // GET: GmailSetup
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(EmailService.Models.Email model)
+        {
+            MailMessage mm = new MailMessage("gmailaddress",model.To);
+            mm.Subject = model.Subject;
+            mm.Body = model.Body;
+            mm.IsBodyHtml = false;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+
+            NetworkCredential nc = new NetworkCredential("gmailaddress", "password");
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = nc;
+            smtp.Send(mm);
+            ViewBag.Message = "Mail has been sent";
+
+            return View();
+        }
+
+    }
+}
